bbbd5b578d595ddd8ca682a64524f7361a4a28bf
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-21 21:22:45 +0300

    Func Delegate

---
+++ 2-More About Delegation/2-Func Delegate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    //Method 1
+    delegate double Square(double x);
+    static public double SquareNumber(double x)
+    {
+        return x * x;
+    }
+
+
+
+
+    //Method 2
+    //Parameters is up to 16 parameter, and last parameter is the return type.
+    public static Func<int, int, double> Divide = DivideNumbers;
+
+    static public double DivideNumbers(int x, int y)
+    {
+        return x / y;
+    }
+
+
+
+    static void Main(string[] args)
+    {
+
+        //Without Func:
+        Square Square = new Square(SquareNumber);
+        double Result = Square(2);
+        Console.WriteLine("Result = {0}", Result);
+
+
+
+        //Using Func:
+        Result = Divide(10, 2);
+        Console.WriteLine("Result = {0}", Result);
+
+
+
+        Console.ReadKey();
+
+    }
+}
+
+
+
