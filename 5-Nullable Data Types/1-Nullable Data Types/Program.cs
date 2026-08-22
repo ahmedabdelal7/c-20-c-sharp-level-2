b16792cab062edbbc118fa5db06f969180b24fa6
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-22 23:51:34 +0300

    Nullable Values in C#

---
+++ 5-Nullable Data Types/1-Nullable Data Types/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+
+using c = System.Console;
+public class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        //Declare a Nullable int using ( Nullable<T> )
+        Nullable<int> nullableInt1 = null;
+
+        //Short-Hand notation using int? 
+        int? nullableInt2 = null;
+
+        nullableInt2 = 10;
+
+        if (nullableInt1.HasValue) 
+        {
+            c.WriteLine("nullableInt1 has a value: {0}",nullableInt1.Value);
+        }
+        else
+        {
+            c.WriteLine("nullableInt1 has no value.");
+
+        }
+        c.WriteLine();
+
+        if (nullableInt2.HasValue)
+        {
+            c.WriteLine("nullableInt2 has a value: {0}", nullableInt2.Value);
+        }
+        else
+        {
+            c.WriteLine("nullableInt2 has no value.");
+
+        }
+
+        // Using the null-coalescing operator 
+        int result = nullableInt2 ?? 0;
+        c.WriteLine("Using null-coalescing operator: " + result);
+
+
+        // Using the null-conditional operator (?.)
+        string stringValue = nullableInt2?.ToString();
+        Console.WriteLine("String representation: " + (stringValue ?? "null"));
+
+        /****************************************************************/
+
+        DateTime? dateTime1 = DateTime.UtcNow;
+
+        //if datetime = null dont invoke ToString method and else return "No Date"
+        string strDate = dateTime1?.ToString() ?? "No Date";
+
+        c.WriteLine(strDate);
+
+        c.ReadKey();
+
+    }
+}
---
+++ 5-Nullable Data Types/2-Nullable Data Types - Example/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+
+using c = System.Console;
+public class Program
+{
+
+    static void Procedure1(string Name, Nullable<int> Age)
+    {
+        c.WriteLine("Name Is : " + Name);
+
+        if (Age.HasValue)
+        {
+            c.WriteLine("Age Is: "+Age.Value);
+        }
+        else
+        {
+            c.WriteLine("Age Is: Null");
+        }
+    }
+    
+    static void Procedure2(string Name, int? Age)
+    {
+        c.WriteLine("Name Is : " + Name);
+
+        //string age = Age?.ToString();
+
+        c.WriteLine($"Age Is: {Age?.ToString() ?? "Null"}");
+
+        //if (Age.HasValue)
+        //{
+        //    c.WriteLine("Age Is: " + Age.Value);
+        //}
+        //else
+        //{
+        //    c.WriteLine("Age Is: Null");
+        //}
+    }
+
+    static void Main(string[] args)
+    {
+        Procedure1("Ahmed Abdelaal",null);
+        Procedure1("Ahmed Abdelaal",22);
+        c.WriteLine();
+        Procedure2("Ahmed Abdelaal", null);
+        Procedure2("Ahmed Abdelaal", 22);
+
+
+        c.ReadKey();
+
+    }
+}
