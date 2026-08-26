6c97f4d40f04a1b09a19a5dcadbb5408f6238eed
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-26 23:29:35 +0300

    Add Description to a Method

---
+++ 9-Add Description to a Method/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+/// This class is a simple calculator.
+/// </summary>
+
+public class Calculator
+{
+    /// <summary>
+    /// This method to add tow numbers.
+    /// </summary>
+    /// <param name="num1">first int parameter</param>
+    /// <param name="num2">second int parameter</param>
+    /// <returns>Sum of tow numbers</returns>
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    /// <summary>
+    /// This method to subtract tow numbers.
+    /// </summary>
+    /// <param name="num1">first int parameter</param>
+    /// <param name="num2">second int parameter</param>
+    /// <returns>Subtract first parameter from the second.</returns>
+    public int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+}
+class Program
+{        
+    static void Main()
+    {
+        
+        Calculator calculator = new Calculator();
+        
+        Console.WriteLine("Result = "+ calculator.Add(5, 5));
+        Console.WriteLine("Result = "+ calculator.Subtract(8,2));
+        
+        Console.ReadKey();
+
+    }
+}
+
