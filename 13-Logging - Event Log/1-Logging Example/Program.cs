5a3f66a0e4cfdfd5fc72a2922b96bad378c0f547
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-28 00:01:04 +0300

    Event Log

---
+++ 13-Logging - Event Log/1-Logging Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        string sourceName = "MyApp";
+
+        //Required Privileges || Permissions
+        if (!EventLog.SourceExists(sourceName))
+        {
+            EventLog.CreateEventSource(sourceName, "Application");
+            Console.WriteLine("Event source created.");
+        }
+
+        //Write information
+        EventLog.WriteEntry(sourceName,"This is my information!",EventLogEntryType.Information);
+
+        //Write warning
+        EventLog.WriteEntry(sourceName, "This is my warning!", EventLogEntryType.Warning);
+
+        //Write Error
+        EventLog.WriteEntry(sourceName, "This is my Error!", EventLogEntryType.Error);
+
+        Console.ReadKey();
+    }
+}
