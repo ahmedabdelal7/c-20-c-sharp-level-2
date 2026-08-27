22a01b4661ef86906e65f354c8b3b11072a77479
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-28 01:07:45 +0300

    App.config

---
+++ 14-App.config/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+            string LogLevel = ConfigurationManager.AppSettings.Get("LogLevel");
+            string Koko = ConfigurationManager.AppSettings["Koko"];
+
+            string ConnectionString2 = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+
+            Console.WriteLine(ConnectionString);
+            Console.WriteLine(LogLevel);
+            Console.WriteLine(Koko);
+            Console.WriteLine(ConnectionString2);
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+}
