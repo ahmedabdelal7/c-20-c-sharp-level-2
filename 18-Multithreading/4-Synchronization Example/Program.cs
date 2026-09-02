70b0523e876806277fb3641b8977a63cd6af3080
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-09-02 17:21:04 +0300

    What is Thread Class

---
+++ 18-Multithreading/4-Synchronization Example/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System;
+
+class Program
+{
+    static int sharedCounter = 0;
+    static object lockObject = new object();
+
+
+    static void Main()
+    {
+        // Create two threads that increment a shared counter
+        Thread t1 = new Thread(() => IncrementCounter("T1"));
+        Thread t2 = new Thread(() => IncrementCounter("T2"));
+
+
+        t1.Start();
+        t2.Start();
+
+
+        // Wait for both threads to complete
+        t1.Join();
+        t2.Join();
+
+
+        Console.WriteLine("Final Counter Value: " + sharedCounter);
+        Console.ReadKey();
+    }
+
+    /*
+     ثريد واحد يعدل عليه ويخلص بعدين يجي الثاني lock  الهدف من 
+     يعنى تناوب مو انو لازم الثريد يخلص كلو بعدين يجي الثاني       
+     */
+    static void IncrementCounter(string ThreadName)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            // Use lock to synchronize access to the shared counter
+            lock (lockObject)
+            {
+                sharedCounter++;
+
+                Console.WriteLine(ThreadName + " : " + sharedCounter);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ 19- Asynchronous Programming/1-What is Thread Class/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+//Task Parallel Library (TPL).
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    static async Task Main()
+    {
+        // Create and run an asynchronous task
+        Task<bool> resultTask = PerformAsyncOperation();
+
+        // Do some other work while waiting for the task to complete
+        Console.WriteLine("Doing some other work...");
+
+
+        // Wait for the task to complete and retrieve the result
+        bool result = await resultTask;
+
+        // Process the result
+        Console.WriteLine($"Result: {result}");
+        Console.ReadKey();
+    }
+
+
+    static async Task<bool> PerformAsyncOperation()
+    {
+        // Simulate an asynchronous operation
+        //await Task.Delay(4000);
+
+        Console.WriteLine("Start Writing to file task..... I will do something else while writing task finish.");
+        await Task.Run(() => { WriteToFIle();});
+        Console.WriteLine("Done Writing to file.");
+
+        // Return a result
+        return true;
+    }
+
+    static void WriteToFIle()
+    {
+        File.WriteAllText("file.txt",
+            "***************************" +
+            "***************************" +
+            "***************************");
+    }
+}
